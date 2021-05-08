1765237305c6d859eaf5b96bddd708e024e3897c
smckendry87 <83869784+smckendry87@users.noreply.github.com>
2021-05-08 15:22:43 +0100

    Add files via upload

---
+++ master.js
@@ -0,0 +1,34 @@
+// When the user clicks on the button, open the modal
+function modal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+window.onload = function() {
+    setTimeout(modal, 2000);
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Hamburger Menu
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
